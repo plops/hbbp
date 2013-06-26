a3cf7f25cd74cd328dcdeb56869102906253abb2
Jan Huwald <jh@sotun.de>
2013-06-26 17:47:13 +0200

    dissector: allow zero length tasks, reformat

--- wireshark/hbbp_dissector.lua
+++ wireshark/hbbp_dissector.lua
@@ -1,24 +1,22 @@
-hbbp_proto = Proto("hbbp","Home Brew Broadcast Protocol")
+hbbp_proto = Proto("hbbp", "Home Brew Broadcast Protocol")
+
 -- create a function to dissect it
-function hbbp_proto.dissector(buffer,pinfo,tree)
+function hbbp_proto.dissector(buffer, pinfo, tree)
     pinfo.cols.protocol = "HBBP"
-    local subtree = tree:add(hbbp_proto,buffer(),"HBBP Data")
-    
+    local subtree = tree:add(hbbp_proto, buffer(), "HBBP Data")
+
+    -- find the \0 that seperates task and payload
     local i = 0
     local b = buffer():bytes()
-    
-    while (i<b:len() and b:get_index(i)~=0) do
+    while i < b:len() and b:get_index(i) ~= 0 do
       i = i + 1
     end
-    if i==0 then
-       return (nil)
-    end
-    subtree:add(buffer(0,i),"Task: " .. buffer(0,i):string())   
+
+    subtree:add(buffer(0, i), "Task: " .. buffer(0, i):string())   
     if b:get_index(i) == 0 then
-       subtree:add(buffer(i+1),"Payload: [Length " .. buffer:len()-i .. "]")   
+       subtree:add(buffer(i + 1),"Payload [Length " .. buffer:len() - i .. "]")   
     end
 end
--- load the udp.port table
-udp_table = DissectorTable.get("udp.port")
+
 -- register our protocol to handle udp port 4950
-udp_table:add(4950,hbbp_proto)
+DissectorTable.get("udp.port"):add(4950, hbbp_proto)
